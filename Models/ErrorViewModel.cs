580e1e031e702e2be7a717195045dab98e5e25ae
Victor Eiras <victor.eiras@gmail.com>
2024-01-17 13:36:29 -0300

    Versão padrão do Asp.Net Core MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace aspnet_core_mvc_crud.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
