b15d53da595ca56dd72e95e1dade7e555bcd33b8
Victor Eiras <victor.eiras@gmail.com>
2024-01-17 13:58:35 -0300

    Criação da entitade Contato no Model

---
+++ Models/Contato.cs
@@ -0,0 +1,10 @@
+namespace aspnet_core_mvc_crud.Models;
+
+public class Contato
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Sobrenome { get; set; }
+    public string Telefone { get; set; }
+    public string Email { get; set; }
+}
